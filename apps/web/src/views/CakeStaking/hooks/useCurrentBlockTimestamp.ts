04ab03c278957ce5494e588060bf39234562ca93
9mmdev <ninemmdev@gmail.com>
2025-04-23 01:30:21 +0530

    typescript fixes

--- apps/web/src/views/CakeStaking/hooks/useCurrentBlockTimestamp.ts
+++ apps/web/src/views/CakeStaking/hooks/useCurrentBlockTimestamp.ts
@@ -1,12 +1,11 @@
+import { useActiveChainId } from 'hooks/useActiveChainId'
 import { useCurrentBlockTimestamp as useBlockTimestamp } from 'state/block/hooks'
 import { verifyBscNetwork } from 'utils/verifyBscNetwork'
-import { useActiveChainId } from 'hooks/useActiveChainId'
-import { ChainId } from '@pancakeswap/chains'
 
 export const useCurrentBlockTimestamp = () => {
   const { chainId } = useActiveChainId()
   const isBscNetwork = verifyBscNetwork(chainId)
-  const timestamp = useBlockTimestamp(!isBscNetwork ? ChainId.BSC : undefined)
+  const timestamp = useBlockTimestamp()
 
   return timestamp ?? 0
 }
