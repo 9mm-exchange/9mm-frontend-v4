8ad945fcbf0581912e2ab1e413845a102f6414c5
Deva Prasad <devaprasad@Devas-MacBook-Air-2.local>
2025-12-18 20:53:26 +0530

    feat: Update logo icons

--- packages/uikit/src/components/Svg/Icons/Logo.tsx
+++ packages/uikit/src/components/Svg/Icons/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SvgProps } from "../types";
 
 const Icon: React.FC<React.PropsWithChildren<SvgProps>> = (props) => {
-  return <img src="/logo.png?hash=178d" alt="9MM Exchange" width="32px" />;
+  return <img src="/logo.png" alt="9MM Exchange" width="32px" />;
 };
 
 export default Icon;
--- packages/uikit/src/components/Svg/Icons/LogoWithText.tsx
+++ packages/uikit/src/components/Svg/Icons/LogoWithText.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../types";
 const Logo: React.FC<React.PropsWithChildren<SvgProps>> = (props) => {
   const theme = useTheme();
 
-  const logoSrc = theme.isDark ? "/logo.png?hash=178d" : "/logo-light.png?hash=178d";
+  const logoSrc = theme.isDark ? "/logo.png" : "/logo-light.png";
 
   return (
     <>
